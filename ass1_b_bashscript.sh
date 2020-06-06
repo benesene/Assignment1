ffcf4eabc1c3a0d6cf7caeebd86ac28edcd75a61
benesene <59637409+benesene@users.noreply.github.com>
2020-06-05 23:30:38 -0700

    Delete ass1_b_bashscript.sh

--- ass1_b_bashscript.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-csvcut -c Dest flightdelay2007.csv |sort | uniq -c | sort -nr |awk 'BEGIN{print "Dest""-""Count"};{print $2"-"$1}'| head -4 > top3dest.csv
-
-csvlook top3dest.csv
